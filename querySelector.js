7e9c868285e29f59629f6ec6b220bcb1d7aa2fa1
cobbdb <cobbdb@gmail.com>
2014-08-03 01:27:54 -0700

    reference original author

---
+++ querySelector.js
@@ -0,0 +1,35 @@
+/**
+ * Polyfills the querySelector and querySelectorAll methods.
+ * @see https://gist.github.com/Fusselwurm/4673695
+ */
+(function () {
+    var style = document.createStyleSheet(),
+        select = function (selector, maxCount) {
+            var all = document.all,
+                l = all.length,
+                i,
+                resultSet = [];
+ 
+            style.addRule(selector, "foo:bar");
+            for (i = 0; i < l; i += 1) {
+                if (all[i].currentStyle.foo === "bar") {
+                    resultSet.push(all[i]);
+                    if (resultSet.length > maxCount) {
+                        break;
+                    }
+                }
+            }
+            style.removeRule(0);
+            return resultSet;
+        };
+    if (document.querySelectorAll || document.querySelector) {
+        return;
+    }
+
+    document.querySelectorAll = function (selector) {
+        return select(selector, Infinity);
+    };
+    document.querySelector = function (selector) {
+        return select(selector, 1)[0] || null;
+    };
+}());
